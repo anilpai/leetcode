980af3442afeef459468b381ec3a5505a4275a2e
anilpai <anilbpai@gmail.com>
2019-12-17 13:45:40 -0600

    Improved code for LCA

--- BalancedBT/LCA_BT.py
+++ BalancedBT/LCA_BT.py
@@ -1,4 +1,4 @@
-from TreeSerialize.TreeSerialize import serialize, deserialize, drawtree
+from TreeSerialize.TreeSerialize import deserialize, drawtree
 
 
 '''
@@ -6,9 +6,9 @@ Needs Python 3+
 '''
 
 
-class Solution(object):
+class Solution_old(object):
     '''
-    Lowest Common Ancestor (LCA) in a Binary Tree (BT)
+    Lowest Common Ancestor (LCA) in a Binary Tree (BT)  : Takes additional space, not space optimized.
     '''
 
     def findPath(self, root, path, k):
@@ -56,12 +56,55 @@ class Solution(object):
         return path1[i-1]
 
 
+class Solution(object):
+    '''
+    Lowest Common Ancestor (LCA) in a Binary Tree (BT)
+    '''
+
+    def lca(self, root, p, q):
+        if root is None:
+            return None
+
+        left = self.lca(root.left, p, q)
+        right = self.lca(root.right, p, q)
+
+        if (left and right) or (root in [p, q]):
+            return root
+        else:
+            return left or right
+
+
 if __name__ == '__main__':
-    s = Solution()
-    lca = s.lca(deserialize('[1,2,3,null,null,4,null,5,6]'), 5, 6)
-    print(lca)
-    drawtree(deserialize('[1,2,3,null,null,4,null,5,6]'))
-    print(s.lca(deserialize('[2,1,3,0,7,9,11,12,null,21,20,null,null,38,48,null,null,null,null,17]'), 17, 12))
-    drawtree(deserialize('[2,1,3,0,7,9,11,12,null,21,20,null,null,38,48,null,null,null,null,17]'))
-    print(s.lca(deserialize('[2,1,3,0,7,9,11,12,null,21,20,null,null,18,28,null,null,null,null,17,22,31,43,0,47,49,51,52,null,61,40,null,null,48,58,null,null,null,null,47]'), 21, 58))
-    drawtree(deserialize('[2,1,3,0,7,9,11,12,null,21,20,null,null,18,28,null,null,null,null,17,22,31,43,0,47,49,51,52,null,61,40,null,null,48,58,null,null,null,null,47]'))
+
+    """ Both p & q must exist. If either of them is null, then the other node is the least common ancestor. """
+
+    old_solution = False
+
+    if old_solution:
+        s = Solution_old()
+        lca = s.lca(deserialize('[1,2,3,null,null,4,null,5,6]'), 5, 6)
+        print(lca)
+        drawtree(deserialize('[1,2,3,null,null,4,null,5,6]'))
+        print(s.lca(deserialize('[2,1,3,0,7,9,11,12,null,21,20,null,null,38,48,null,null,null,null,17]'), 17, 12))
+        drawtree(deserialize('[2,1,3,0,7,9,11,12,null,21,20,null,null,38,48,null,null,null,null,17]'))
+        print(s.lca(deserialize('[2,1,3,0,7,9,11,12,null,21,20,null,null,18,28,null,null,null,null,17,22,31,43,0,47,49,51,52,null,61,40,null,null,48,58,null,null,null,null,47]'), 21, 58))
+        drawtree(deserialize('[2,1,3,0,7,9,11,12,null,21,20,null,null,18,28,null,null,null,null,17,22,31,43,0,47,49,51,52,null,61,40,null,null,48,58,null,null,null,null,47]'))
+    else:
+        s1 = Solution()
+
+        # Example 1
+        # root = deserialize('[1,2,3,null,null,4,null,5,6]')
+        # p = root.right.left.left
+        # q = root.right.left.right
+        # lca = s1.lca(root, p, q)
+        # print(lca)
+        # drawtree(deserialize('[1,2,3,null,null,4,null,5,6]'))
+
+        # Example 2
+        root = deserialize('[2,1,3,0,7,9,11,12,null,21,20,null,null,38,48,null,null,null,null,17]')
+        p = root.left.left.left
+        q = root.left.right.right.left
+        lca = s1.lca(root, p, q)
+        print(lca)
+        drawtree(deserialize('[2,1,3,0,7,9,11,12,null,21,20,null,null,38,48,null,null,null,null,17]'))
+
